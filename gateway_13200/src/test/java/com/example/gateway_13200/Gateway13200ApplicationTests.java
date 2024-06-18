15e6f914b901b1520f9cfa10d717ddd653887920
zhixuanzhou <2630526002@qq.com>
2024-06-18 19:26:28 +0800

    gateway模块

---
+++ gateway_13200/src/test/java/com/example/gateway_13200/Gateway13200ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gateway_13200;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gateway13200ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
